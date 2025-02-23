54a9ca7bde6f020da9eaf5ca69e406322fe4b4d7
Priyanka Mishra <priyanka.mishra@example.com>
2025-02-23 15:12:27 +0530

    Updated code

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import React from "react"
-import Nav from "./Component/Navbar";
-function App() {
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
+import Nav from './Component/Navbar';
+import SignIN from './Component/Sign';
+
+const App = () => {
   return (
-  <div>
-    <Nav/>
-  </div>
-  )
+    <Router>
+    {/* <Nav/> */}
+      <Routes>
+        {/* Home page par Navbar dikhana hai */}
+        <Route path="/" element={<Nav/>} />
+        
+        {/* SignIN page fullscreen hona chahiye, isliye yahan Navbar nahi hai */}
+        <Route path="/sign" element={<SignIN />} />
+        
+      </Routes>
+    </Router>
+  );
 }
 
-export default App;;
+export default App;
