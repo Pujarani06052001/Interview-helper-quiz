07542d4c792d3df5ba87ae2ba52b01b14d0556fa
Priyanka Mishra <priyanka.mishra@example.com>
2025-02-23 17:10:12 +0530

    upadte all files

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,15 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Nav from './Component/Navbar';
 import SignIN from './Component/Sign';
+import Home from './Component/Home'; 
 
 const App = () => {
   return (
     <Router>
-    {/* <Nav/> */}
+      <Nav />
       <Routes>
-        {/* Home page par Navbar dikhana hai */}
-        <Route path="/" element={<Nav/>} />
-        
-        {/* SignIN page fullscreen hona chahiye, isliye yahan Navbar nahi hai */}
+        <Route path="/" element={<Home />} />  
         <Route path="/sign" element={<SignIN />} />
-        
       </Routes>
     </Router>
   );
