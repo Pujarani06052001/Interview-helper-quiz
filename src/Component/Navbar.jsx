3a28c6151e5983fec725e0b95bd435693b5ea590
Poojarani <ranipooja21@navgurukul.org>
2025-02-23 22:58:17 +0530

    resolve error

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,25 +1,21 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-// import { Link } from "react-router-dom";
-import "./Navbar.css";  
+import "./Navbar.css";
 
 function Nav() {
   const navigate = useNavigate();
+  
   return (
-    <>
-      <header className="header">
-        <div className="container">
-          <h1 className="container-h1">Mock Interview</h1>
-          <nav className="container-nav">
-            <div className="nav-usediv">
-              <button className="dashboard" onClick={() => navigate("/sign")}>
-            Sign In 
-          </button>
-            </div>
-          </nav>
-        </div>
-      </header>
-    </>
+    <header className="header">
+      <div className="container">
+        <h1 className="container-h1">Mock Interview</h1>
+        <nav className="container-nav">
+          <div className="nav-usediv">
+            <button className="dashboard" onClick={() => navigate("/sign")}>Sign In</button>
+          </div>
+        </nav>
+      </div>
+    </header>
   );
 }
 
